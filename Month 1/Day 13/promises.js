d0ecb22cbd2c0a950ad6d3216d28935d266b49f2
hanan.naseer <hanan.naseer@lelafe.com>
2025-06-05 16:00:29 +0530

    promise chaining and error handling

--- Month 1/Day 13/promises.js
+++ Month 1/Day 13/promises.js
@@ -38,15 +38,51 @@
 
 // Example 3
 
-function getImages(image) {
+// function getImages(image) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             const data = readFile(image);
+//             resolve(data)
+//         } catch (error) {
+//             reject(new Error(error))
+//         }
+//     })
+// }
+
+// getImages('./image1.png')
+
+const cart = ["shoes", "pants", "kurta"];
+
+
+function validateCart(cart) {
+    return true;
+}
+
+function createOrder(cart) {
     return new Promise((resolve, reject) => {
-        try {
-            const data = readFile(image);
-            resolve(data)
-        } catch (error) {
-            reject(new Error(error))
+        //create orderid
+        if (!validateCart(cart)) {
+            const error = new Error("Cart is not valid");
+            reject(error)
         }
+        const orderId = 123;
+        setTimeout(() => {
+            resolve(orderId)
+        }, 1000)
     })
 }
 
-getImages('./image1.png')
+function proceedToPayment(orderId) {
+    return new Promise((resolve, reject) => {
+        resolve("Payment successfull")
+    })
+}
+
+const promise = createOrder(cart);
+
+promise
+    .then((orderId) => console.log(orderId))
+    // .catch(error => console.log(error.message)) handles only above then and rest then will still work
+    .then((orderId) => proceedToPayment(orderId))
+    .then((message) => console.log(message))
+    .catch(error => console.log(error.message))
