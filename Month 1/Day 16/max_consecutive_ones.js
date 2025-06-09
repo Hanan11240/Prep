d594ce91b38f591cab15035b5c40f30a71c91a00
hanan11240 <peerzadahanannaseer@gmail.com>
2025-06-09 15:46:33 +0530

    another similar question

--- Month 1/Day 16/max_consecutive_ones.js
+++ Month 1/Day 16/max_consecutive_ones.js
@@ -18,3 +18,24 @@ function consequetiveOnes(arr) {
 
 
 console.log(consequetiveOnes([1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1]))
+
+
+
+// maximum count of any el in array
+// // function maxConsecutiveCount(arr) {
+//     let maxCount = 0, count = 1;
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] === arr[i - 1]) {
+//             count++;
+//         } else {
+//             maxCount = Math.max(maxCount, count);
+//             count = 1;
+//         }
+//     }
+
+//     return Math.max(maxCount, count);
+// }
+
+// let arr = [1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1];
+// console.log(maxConsecutiveCount(arr));
