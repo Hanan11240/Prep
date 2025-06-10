0820a3607b989422af572114f1c37b64a1654fce
hanan11240 <peerzadahanannaseer@gmail.com>
2025-06-10 21:04:07 +0530

    deep clone

--- Month 1/Day 17/Clone_deeply.js
+++ Month 1/Day 17/Clone_deeply.js
@@ -1,2 +1,31 @@
+function deepClone(obj){
+    if(obj === null || typeof obj !== 'object'){
+        return obj
+    }
+    if(Array.isArray(obj)){
+        return obj.map(deepClone);
+    }
 
+    const clone = {};
 
+    for(let key in obj){
+        clone[key] = deepClone(obj[key]);
+    }
+
+    return clone
+}
+
+const original = {
+  name: "Ali",
+  nested: {
+    age: 25,
+    hobbies: ["reading", "gaming"]
+  }
+};
+
+const copy = deepClone(original);
+copy.nested.age = 30;
+copy.nested.hobbies[0] = "writing";
+
+console.log(original);     
+console.log(copy);
