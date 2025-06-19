74d13676083cc6cca5b5be7cf6bb564f0c306df1
hanan.naseer <hanan.naseer@lelafe.com>
2025-06-19 10:56:24 +0530

    add getter

---
+++ Month 1/Day 28/get.js
@@ -0,0 +1,30 @@
+// In JavaScript, there are two kinds of object properties:
+
+// Data properties
+// Accessor properties
+
+
+// const student = {
+
+    // data property
+//     firstName: 'Monica';
+// }
+
+
+// Accessor Property
+// In JavaScript, accessor properties are methods that get or set the value of an object. For that, we use these two keywords:
+
+// get - to define a getter method to get the property value
+// set - to define a setter method to set the property value
+
+
+const student = {
+    firstName:"Hanan",
+    get getName(){
+        return this.firstName
+    }   
+}
+
+
+// console.log(student.getName)
+// console.log(student.getName()) //error
