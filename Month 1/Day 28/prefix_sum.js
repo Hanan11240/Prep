3b6ccae00cb98e18b3998bae3c534e28e7793c0e
hanan.naseer <hanan.naseer@lelafe.com>
2025-06-20 12:02:41 +0530

    add new files

--- Month 1/Day 28/prefix_sum.js
+++ Month 1/Day 28/prefix_sum.js
@@ -1,13 +1,17 @@
 // create prefix sum
+// It helps to quickly calculate the sum of elements between two indices l and r in an array:
+
+
+
 
 function prefixSum(arr) {
 
     let preFixArray = new Array(arr.length);
     preFixArray[0] = arr[0];
-    let prefixSum = 0;
+
     for (let i = 1; i < arr.length; i++) {
-        prefixSum = preFixArray[i - 1] + arr[i];
-        preFixArray[i] = prefixSum;
+        preFixArray[i] = preFixArray[i - 1] + arr[i];
+
     }
 
     return preFixArray
@@ -17,4 +21,18 @@ function prefixSum(arr) {
 
 
 
+
+
+
 console.log(prefixSum([1, 2, 3, 4, 5, 6]))
+
+// Common Use Cases
+// Range Sum Queries (as explained above)
+
+// Finding number of 1s in binary subarrays
+
+// Cumulative frequency or probability
+
+// 2D Prefix Sum for submatrix sum queries
+
+// Difference arrays (inverse of prefix sum)
