49971c72474119532b01390b5b4ee579a2311c8f
hanan.naseer <hanan.naseer@lelafe.com>
2025-06-24 15:49:25 +0530

    add binary search

---
+++ Month 2/Day 2/binary_search.js
@@ -0,0 +1,23 @@
+
+function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length - 1;
+   
+    while (low <= high) {
+         let mid = Math.floor((low + high) / 2);
+        if (arr[mid] === target) {
+            return mid
+        } else if (arr[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1
+        }
+    }
+    return -1
+}
+
+
+
+
+
+console.log(binarySearch([1, 4, 21, 30, 1000], 0))
