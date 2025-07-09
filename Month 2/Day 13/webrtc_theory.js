8972749d43c313ef8045364e588206420de3ddd6
hanan.naseer <hanan.naseer@lelafe.com>
2025-07-09 14:41:02 +0530

    add webrtc theory link

---
+++ Month 2/Day 13/webrtc_theory.js
@@ -0,0 +1,4 @@
+// https://webrtcforthecurious.com/
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Simple_RTCDataChannel_sample
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API
